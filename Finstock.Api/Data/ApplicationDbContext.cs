c19a5e1339466fcca13312a6dbb6c1824a4a2324
afsal ms <afsalms715@gmail.com>
2024-02-15 21:07:06 +0300

    Add project files.

---
+++ Finstock.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Finstock.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finstock.Api.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions optoins):base(optoins){}
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
---
+++ Finstock.Api/Models/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Finstock.Api.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string ComponyName { get; set; } = string.Empty;
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Purchase { get; set; }
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public int MarketCap { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
