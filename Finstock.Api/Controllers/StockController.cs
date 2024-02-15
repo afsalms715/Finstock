cbd441f371c5119cd9a0ee98b4c90f6926420396
afsal ms <afsalms715@gmail.com>
2024-02-15 21:28:27 +0300

    stock controller added

---
+++ Finstock.Api/Controllers/StockController.cs
@@ -0,0 +1,34 @@
+﻿using Finstock.Api.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finstock.Api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public StockController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var stocks = context.Stocks.ToList();
+            return Ok(stocks);
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            var stock = context.Stocks.Find(id);
+            if(stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
+    }
+}
