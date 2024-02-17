2b559853831f449f9732a63289885eac3ae43d9f
afsal ms <afsalms715@gmail.com>
2024-02-17 21:56:35 +0300

    implimeented Async

--- Finstock.Api/Controllers/StockController.cs
+++ Finstock.Api/Controllers/StockController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Finstock.Api.Mappers;
 using Finstock.Api.DTOs.Stock;
 using System.Net.WebSockets;
+using Microsoft.EntityFrameworkCore;
 
 namespace Finstock.Api.Controllers
 {
@@ -19,18 +20,19 @@ namespace Finstock.Api.Controllers
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
-            var stocks = context.Stocks.ToList().Select(s=>s.ToStockDto());
-            return Ok(stocks);
+            var stocks = await context.Stocks.ToListAsync();
+            var stockDtos=stocks.Select(s => s.ToStockDto());
+            return Ok(stockDtos);
         }
 
         [HttpGet("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult GetById(int id)
+        public async Task<IActionResult> GetById(int id)
         {
-            var stock = context.Stocks.Find(id);
+            var stock = await context.Stocks.FindAsync(id);
             if(stock == null)
             {
                 return NotFound();
@@ -42,25 +44,25 @@ namespace Finstock.Api.Controllers
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status201Created)]
-        public IActionResult CreateStock(CreateStockDto createStockDto)
+        public async Task<IActionResult> CreateStock(CreateStockDto createStockDto)
         {
-            var IsDuplicate=context.Stocks.FirstOrDefault(u=>u.Symbol==createStockDto.Symbol);
+            var IsDuplicate=await context.Stocks.FirstOrDefaultAsync(u=>u.Symbol==createStockDto.Symbol);
             if(IsDuplicate != null)
             {
                 ModelState.AddModelError("Duplicate Data", "this Symbol is already exist");
                 return BadRequest(ModelState);
             }
             var stock = createStockDto.ToStockFromCreateStock();
-            context.Stocks.Add(stock);
-            context.SaveChanges();
+            await context.Stocks.AddAsync(stock);
+            await context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetById),new {id=stock.Id},stock.ToStockDto());
         }
 
         [HttpPut("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult UpdateStock([FromRoute] int id, [FromBody] UpdateStockDto stockDto) {
-            var stock = context.Stocks.Find(id);
+        public async Task<IActionResult> UpdateStock([FromRoute] int id, [FromBody] UpdateStockDto stockDto) {
+            var stock =await context.Stocks.FindAsync(id);
             if(stock == null)
             {
                 return NotFound(ModelState);
@@ -70,22 +72,22 @@ namespace Finstock.Api.Controllers
             stock.Purchase = stockDto.Purchase;
             stock.LastDiv=stockDto.LastDiv;
             stock.Industry = stockDto.Industry;
-            context.SaveChanges();
-            return Ok(stock);
+            await context.SaveChangesAsync();
+            return Ok(stock.ToStockDto());
         }
 
         [HttpDelete("{id:int}")]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public IActionResult DeleteStock([FromRoute] int id)
+        public async Task<IActionResult> DeleteStock([FromRoute] int id)
         {
-            var stock = context.Stocks.Find(id);
+            var stock = await context.Stocks.FindAsync(id);
             if(stock == null)
             {
                 return NotFound();
             }
             context.Stocks.Remove(stock);
-            context.SaveChanges();
+            await context.SaveChangesAsync();
             return NoContent();
         }
     }
