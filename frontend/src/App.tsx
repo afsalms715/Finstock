654dd1f77c88bbe598f1d1fad948f0203301fe17
afsal ms <afsalms715@gmail.com>
2024-03-02 22:43:00 +0530

    forntend started

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import Card from './components/Card/Card.tsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <p>testing</p>
+     <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Card/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Card = (props: Props) => {
+  return (
+    <div className='card'>
+        <img src='https://i.ibb.co/WBWWD4h/685-6854994-react-logo-no-background-hd-png-download.png" alt="685-6854994-react-logo-no-background-hd-png-download' className='h-50 w-20'/>
+    </div>
+  )
+}
+
+export default Card
