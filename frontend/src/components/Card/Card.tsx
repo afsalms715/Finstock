e6234c2eb8555a02dabcca2d9e10239c7f80cbee
afsal ms <afsalms715@gmail.com>
2024-03-03 09:16:33 +0530

    card created

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -3,9 +3,15 @@ import React from 'react'
 type Props = {}
 
 const Card = (props: Props) => {
-  return (
-    <div className='card'>
-        <img src='https://i.ibb.co/WBWWD4h/685-6854994-react-logo-no-background-hd-png-download.png" alt="685-6854994-react-logo-no-background-hd-png-download' className='h-50 w-20'/>
+  return (  
+    <div className="max-w-sm md:w-[20%] rounded overflow-hidden shadow-lg m-1">
+        <img className="w-full" src="https://i.ibb.co/WBWWD4h/685-6854994-react-logo-no-background-hd-png-download.png" alt="Sunset in the mountains"/>
+        <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">Stock Name</div>
+            <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
+        </div>
     </div>
   )
 }
