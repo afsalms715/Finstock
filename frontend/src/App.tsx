220bd3abea7b04b9a61b572ece8be54cf847b0bc
afsal ms <afsalms715@gmail.com>
2024-03-04 21:25:44 +0530

    search box

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Card from './components/Card/Card.tsx'
 import './App.css'
 import CardList from './components/CardList/CardList.tsx'
+import SearchBox from './components/Search/SearchBox.tsx'
 
 function App() {
 
   return (
     <div>
+    <SearchBox/>
      <CardList/>
     </div>
   )
