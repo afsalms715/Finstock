11cf7f4ddf6c78fdba95bda87dbb38d3599ac766
afsal ms <afsalms715@gmail.com>
2024-03-04 16:48:25 +0530

    type mention React.FC and JSX.Element

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -2,19 +2,20 @@ import React from 'react'
 
 interface Props  {
     companyName:string;
-    ticket:string;
+    ticker:string;
     price:number;
+    decription:string;
 }
 
-const Card = ({companyName,ticket,price}: Props) => {
+const Card:React.FC<Props> = ({companyName,ticker,price,decription}):JSX.Element => {
   return (  
     <div className="max-w-sm md:w-[19%] rounded overflow-hidden shadow-lg m-1">
         <img className="w-full" src="https://i.ibb.co/WBWWD4h/685-6854994-react-logo-no-background-hd-png-download.png" alt="Sunset in the mountains"/>
         <div className="px-6 py-4">
-        <div className="text-center font-bold text-xl mb-2">{companyName}</div>
+        <div className="text-center font-bold text-xl mb-2">{companyName} ({ticker})</div>
             <p className='text-center font-bold text-sm'>${price}</p>
             <p className="text-gray-700 text-base">
-                {ticket}
+                {decription}
             </p>
         </div>
     </div>
--- frontend/src/components/CardList/CardList.tsx
+++ frontend/src/components/CardList/CardList.tsx
@@ -3,21 +3,14 @@ import Card from '../Card/Card'
 
 type Props = {}
 
-const CardList = (props: Props) => {
+const CardList:React.FC<Props> = (props:Props):JSX.Element => {
+    let test_desc:string="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure porro esse quibusdam repudiandae voluptatibus qui atque nihil possimus ea deleniti.";
   return (
     <div className='flex flex-wrap justify-center w-[100%]'>
-        <Card companyName='ITC' price={100} ticket='testing description....'/>
-        <Card companyName='ITC' price={100} ticket='testing description....'/>
-        <Card companyName='ITC' price={100} ticket='testing description....'/>
-        <Card companyName='ITC' price={100} ticket='testing description....'/>
-        <Card companyName='ITC' price={100} ticket='testing description....'/>
-        <Card companyName='ITC' price={100} ticket='testing description....'/>
-        <Card companyName='ITC' price={100} ticket='testing description....'/>
-        <Card companyName='ITC' price={100} ticket='testing description....'/>
-        <Card companyName='ITC' price={100} ticket='testing description....'/>
-        <Card companyName='ITC' price={100} ticket='testing description....'/>
-        <Card companyName='ITC' price={100} ticket='testing description....'/>
-        <Card companyName='ITC' price={100} ticket='testing description....'/>
+        <Card companyName='ITC' price={100} ticker='ITC' decription={test_desc}/>
+        <Card companyName='Tata Motors' price={100} ticker='TATM' decription={test_desc}/>
+        <Card companyName='Infosys' price={100} ticker='INF' decription={test_desc}/>
+        
     </div>
   )
 }
